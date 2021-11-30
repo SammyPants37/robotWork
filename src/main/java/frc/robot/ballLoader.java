21adbfcc3308cd4b40b1f610a7ff5d326d490fd8
sammyl182 <94145010+sammyl182@users.noreply.github.com>
2021-11-30 10:49:51 -0600

    some more changes

---
+++ src/main/java/frc/robot/ballLoader.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Spark;
+
+public class ballLoader {
+  private static final Spark Motor = new Spark(1);
+
+  public void setSpeed(double speed) {
+    Motor.setSpeed(speed);
+  }
+
+  public void stop() {
+    Motor.stopMotor();
+  }
+}
