08ed644849f4224f890eed6557b8dfaef76f2ab0
sammyl182 <94145010+sammyl182@users.noreply.github.com>
2021-12-16 09:25:08 -0600

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystem/DriveTrain.java
+++ src/main/java/frc/robot/subsystem/DriveTrain.java
@@ -10,12 +10,6 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveTrain {
-  // private static final double CountsPerRevolution = 1440.0;
-  // private static final double WheelDiameterInch = 2.75591; // 70 mm
-
-  // private final Encoder LEncoder = new Encoder(4, 5);
-  // private final Encoder REncoder = new Encoder(6, 7);
-  
   static Spark m_frontLeft = new Spark(0);
   static Spark m_rearLeft = new Spark(1);
   static SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
@@ -25,18 +19,6 @@ public class DriveTrain {
   static SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
   static DifferentialDrive diffDrive = new DifferentialDrive(m_left, m_right);
 
-  // public void resetEncoders() {
-  //     LEncoder.reset();
-  //     REncoder.reset();
-  // }
-  
-  // public RDriveTrain() {
-  //   // Use inches as unit for encoder distances
-  //   LEncoder.setDistancePerPulse((Math.PI * WheelDiameterInch) / CountsPerRevolution);
-  //   REncoder.setDistancePerPulse((Math.PI * WheelDiameterInch) / CountsPerRevolution);
-  //   resetEncoders();
-  // }
-
   public static void arcadeDrive(double speed, double rotation) {
       diffDrive.arcadeDrive(speed, rotation);
   }
@@ -44,11 +26,4 @@ public class DriveTrain {
   public static void stop() {
     diffDrive.stopMotor();
   }
-  // public double getLeftDistanceInch() {
-  //   return LEncoder.getDistance();
-  // }
-
-  // public double getRightDistanceInch() {
-  //   return REncoder.getDistance();
-  // }
 }
